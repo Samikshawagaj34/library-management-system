379a95c3b8f31a5cae05b18033d640f0a2b67550
Samiksha Wagaj <samikshawagaj2003@gmail.com>
2025-10-09 23:53:43 +0530

    Added book class with encapsulation and getter, setter methods

--- src/LibraryMangementSystem.java
+++ src/LibraryMangementSystem.java
@@ -1,9 +1,50 @@
 // Mini-project : Library Mangement System
 
+class Book{
+	private int id;
+	private String title;
+	private boolean issued;
+	
+	public Book(int id,  String title){
+		this.id = id;
+		this.title = title;
+		this issued = false;
+		
+	}
+	
+	public int getId(){
+		return id
+	}
+	
+	public String getTitl(){
+		return title;
+	}
+	
+	public boolean isIssued(){
+		return issued;
+	} 
+	
+	public void setIssued(boolean issued){
+		this.issued = issued;
+	}
+	
+	public void issue(){
+		setIssued(true);
+		
+	}
+	
+	public void returnBook(){
+		setIssued(false);
+	}
+	
+	public void display(){
+		System.out.println(id + ":" + title + " : Issued :" + issue);
+	}
+}
 
 public class LibraryMangementSystem{
 	public static void main(String[] args){
-		
+	  	
 	}
 	
 
